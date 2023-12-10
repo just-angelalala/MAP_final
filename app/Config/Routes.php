bdf68caba52902118cf819ef6854623def9329be
aronejesrylorosco <aroneorosco@gmail.com>
2023-12-10 19:37:14 +0800

    backend
    backend

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,32 +5,23 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-// $routes->get('/', 'Home::index');
+$routes->get('/', 'Home::index');
 
-// $routes->group('api', static function($routes) {
-//     // Define a route for the Register controller
-//     $routes->post('register', 'RegisterController::register');
+$routes->group('api', static function($routes) {
+    // Define a route for the Register controller
+    $routes->post('register', 'RegisterController::register');
 
-//     $routes->post('login', 'AuthController::login');
-
-
-//     $routes->post('getProductsAndCategories', 'ProductCategoryController::getProductsAndCategories');
-
-
-//     $routes->post('addTransaction', 'AddTransactionController::addTransaction');
-
-//     $routes->get('getAllCategories', 'ProductsController::getAllCategories');
-
-//     $routes->post('addProduct', 'ProductsController::addProduct');
-
-//     $routes->get('displayProducts', 'ProductsController::displayProducts');
-// });
-// Routes.php
-
-$routes->group('api', ['namespace' => 'App\Controllers\API'], function ($routes) {
-    // User registration
-    $routes->post('register', 'UserController::register');
-
-    // User login
     $routes->post('login', 'AuthController::login');
+
+
+    $routes->post('getProductsAndCategories', 'ProductCategoryController::getProductsAndCategories');
+
+
+    $routes->post('addTransaction', 'AddTransactionController::addTransaction');
+
+    $routes->get('getAllCategories', 'ProductsController::getAllCategories');
+
+    $routes->post('addProduct', 'ProductsController::addProduct');
+
+    $routes->get('displayProducts', 'ProductsController::displayProducts');
 });
